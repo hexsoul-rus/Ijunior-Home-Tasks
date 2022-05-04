64b8de2f75f5433e8aa041f3682d16d4f761b1c3
hexsoul-rus <nik_mobil@mail.ru>
2022-05-04 21:24:16 +0300

    new variables

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSharpTask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte age = 18;
+            short laboratoryLevel = -3;
+            ushort room = 15;
+            int populationGrowth = -42000;
+            uint ammo = 300000;
+            bool isDead = false;
+            char separator = '_';
+            string name = "Olga";
+            float moneyBalance = 20.5f;
+            double planetMass = 5.6846E+26;
+        }
+    }
+}
