5b470a48588c9b32e74758f884dcc0f9cde2dba4
hexsoul-rus <nik_mobil@mail.ru>
2022-05-05 10:28:45 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task6Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            uint crystalsPrice = 25;
+            uint gold;
+            uint crystals;
+
+            Console.Write("Введите количество вашего золота: ");
+            gold = Convert.ToUInt32(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("Доро пожаловать в магазин! Чего желаете?");
+            Console.Write("Кристаллы (кол-во): ");
+            crystals = Convert.ToUInt32(Console.ReadLine());
+            gold -= crystals * crystalsPrice;
+            Console.Clear();
+            Console.WriteLine("Теперь у вас " + crystals + " кристраллов и " + gold + " золота. Спасибо за покупку!");
+        }
+    }
+}
