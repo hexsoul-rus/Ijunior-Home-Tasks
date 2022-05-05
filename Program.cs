8056f7ad22d8483ceba9561fe62cc80335172c45
hexsoul-rus <nik_mobil@mail.ru>
2022-05-05 10:51:23 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task7Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte receptionMinutesTime = 10;
+            byte hour = 60;
+            uint queueCount;
+            uint queueHoursTime;
+            uint queueMinutesTime;
+
+            Console.Write("Введите кол-во старушек: ");
+            queueCount = Convert.ToUInt32(Console.ReadLine());
+            queueHoursTime = queueCount * receptionMinutesTime / hour;
+            queueMinutesTime = queueCount * receptionMinutesTime % hour;
+            Console.WriteLine("Вы должны отстоять в очереди " + queueHoursTime + " часа и " + queueMinutesTime + " минут.");
+        }
+    }
+}
