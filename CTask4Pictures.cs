71a1920d07c2899bc409cbd92a1156b8e9523d6e
hexsoul-rus <nik_mobil@mail.ru>
2022-05-05 09:35:28 +0300

    Fix output

--- CTask4Pictures.cs
+++ CTask4Pictures.cs
@@ -8,9 +8,13 @@ namespace Task4Pictures
         {
             uint picturesCount = 52;
             uint series = 3;
+            uint seriesCount;
+            uint extraPictures;
 
-            Console.WriteLine((picturesCount / series) + " полностью заполненных рядов можно вывести.");
-            Console.WriteLine((picturesCount % series) + " картинок будет сверх меры.");
+            seriesCount = picturesCount / series;
+            extraPictures = picturesCount % series;
+            Console.WriteLine(seriesCount + " полностью заполненных рядов можно вывести.");
+            Console.WriteLine(extraPictures + " картинок будет сверх меры.");
         }
     }
 }
