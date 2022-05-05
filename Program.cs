89daed66dbb2eabcbefb4532a679fe29429fa44d
hexsoul-rus <nik_mobil@mail.ru>
2022-05-05 11:47:58 +0300

    Fix var name

--- Program.cs
+++ Program.cs
@@ -9,15 +9,15 @@ namespace Task7Queue
             byte receptionTimeInMinutes = 10;
             byte minutesInHour = 60;
             uint queueSize;
-            uint queueTimeInMinutes;
+            uint queueWaitingTimeInMinutes;
             uint waitingHours;
             uint waitingMinutes;
 
             Console.Write("Введите кол-во старушек: ");
             queueSize = Convert.ToUInt32(Console.ReadLine());
-            queueTimeInMinutes = queueSize * receptionTimeInMinutes;
-            waitingHours = queueTimeInMinutes / minutesInHour;
-            waitingMinutes = queueTimeInMinutes % minutesInHour;
+            queueWaitingTimeInMinutes = queueSize * receptionTimeInMinutes;
+            waitingHours = queueWaitingTimeInMinutes / minutesInHour;
+            waitingMinutes = queueWaitingTimeInMinutes % minutesInHour;
             Console.WriteLine("Вы должны отстоять в очереди " + waitingHours + " часа и " + waitingMinutes + " минут.");
         }
     }
