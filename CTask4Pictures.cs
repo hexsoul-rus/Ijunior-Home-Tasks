347e9af3d0cdb3c4fc24e9486a721eba625c7149
hexsoul-rus <nik_mobil@mail.ru>
2022-05-05 09:10:27 +0300

    Create CTask4Pictures.cs

---
+++ CTask4Pictures.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task4Pictures
+{
+    internal class CTask4Pictures
+    {
+        static void Main(string[] args)
+        {
+            uint picturesCount = 52;
+            uint series = 3;
+
+            Console.WriteLine((picturesCount / series) + " полностью заполненных рядов можно вывести.");
+            Console.WriteLine((picturesCount % series) + " картинок будет сверх меры.");
+        }
+    }
+}
