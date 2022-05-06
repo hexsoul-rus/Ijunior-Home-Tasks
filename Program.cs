d1edeb4e812343a5fd856716896c2109af2f73bc
hexsoul-rus <nik_mobil@mail.ru>
2022-05-06 09:51:53 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task10Subsequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            uint valueStep = 7;
+            uint minValue = 5;
+            uint maxValue = 96;
+
+            for (uint i = minValue; i <= maxValue; i += valueStep)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
