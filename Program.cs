e1d23107871750d7b0ac74f144b9b0d60326dc6a
hexsoul-rus <nik_mobil@mail.ru>
2022-05-06 09:32:19 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task9ExitControl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string commandLine = "";
+            string exitCommand = "exit";
+
+            while (commandLine != exitCommand)
+            {
+                Console.WriteLine("У попа была собака,");
+                Console.WriteLine("Он ее любил.");
+                Console.WriteLine("Она съела кусок мяса,");
+                Console.WriteLine("Он ее убил!");
+                Console.WriteLine("В землю закопал,");
+                Console.WriteLine("Надпись написал, что:\n");
+                Console.Write("Для выхода введите " + exitCommand + " или нажмите Enter, чтобы продолжить: ");
+                commandLine = Console.ReadLine();
+                Console.WriteLine();
+            }
+        }
+    }
+}
