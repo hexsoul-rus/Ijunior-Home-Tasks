519fdf0d4d1fb0873c406b6a3703fcc86be3a334
hexsoul-rus <nik_mobil@mail.ru>
2022-05-06 09:10:41 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task8Cycles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message;
+            uint repetitionsCount;
+
+            Console.Write("Введите сообщение: ");
+            message = Console.ReadLine();
+            Console.Write("Количество повторов: ");
+            repetitionsCount = Convert.ToUInt32(Console.ReadLine());
+            Console.WriteLine();   
+
+            for (int i = 0; i < repetitionsCount; i++)
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
