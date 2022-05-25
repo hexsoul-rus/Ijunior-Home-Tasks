abf6b826f87b068c40cd9ab21204a983c9003d2c
hexsoul-rus <nik_mobil@mail.ru>
2022-05-25 10:41:11 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task26Split
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string wordsLine = "каждый охотник желает знать где сидит фазан";
+            string[] words;
+
+            words = wordsLine.Split(' ');
+
+            foreach (string word in words)
+                Console.WriteLine(word);
+        }
+    }
+}
