5858c50de99145033476039a2340186a00988061
hexsoul-rus <nik_mobil@mail.ru>
2022-06-01 13:15:27 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task30ReadInt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = ReadIntValue();
+            Console.WriteLine($"Введённое целочисленное значение {value}");
+        }
+
+        private static int ReadIntValue()
+        {
+            string enteredValue;
+            int parsedValue;
+            bool isParsed;
+
+            do
+            {
+                Console.Write("Введите целочисленное значение:");
+                enteredValue = Console.ReadLine();
+                isParsed = int.TryParse(enteredValue, out parsedValue);
+
+                if (isParsed == false)
+                    Console.Write("Вводе не является целочисленным значением. ");
+            }
+            while (isParsed == false);
+
+            return parsedValue;
+        }
+    }
+}
