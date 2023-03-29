8a7126468cf72e0f765a68810cb38b273c2555d7
hexsoul-rus <nik_mobil@mail.ru>
2023-03-29 13:18:53 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task32Shuffle
+{
+    internal class Program
+    {
+        static void ShuffleArray(int[] array)
+        {
+            Random random = new();
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int randomIndex = random.Next(i + 1, array.Length - 1);
+                int tmp = array[randomIndex];
+                array[randomIndex] = array[i];
+                array[i] = tmp;
+            }
+        }
+
+        static void Main()
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+
+            ShuffleArray(array);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
+    }
+}
