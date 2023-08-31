62202a3b27adf0937ce7a29cbc14b20357161d95
Andrey Nikitin <nik_mobil@mail.ru>
2023-08-31 21:22:10 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,22 +8,30 @@ namespace Task40PlayersDataBase
     {
         static void Main()
         {
-            ConsoleControl consoleControl = new ();
+            ConsoleControl consoleControl = new ConsoleControl ();
             consoleControl.Run();
         }
     }
 
     public class Player
     {
-        public readonly int Id;
         public string Name {get; private set;}
         public bool IsBanned {get; private set;}
+        public int Id 
+        { 
+            get 
+            { 
+                return _id; 
+            } 
+        }
 
-        public Player(int id, string name) 
+        private int _id;
+
+        public Player(int id, string name, bool isBanned = false) 
         { 
             Name = name;
-            IsBanned = false;
-            Id = id;
+            IsBanned = isBanned;
+            _id = id;
         }
 
         public void Ban()
@@ -31,41 +39,39 @@ namespace Task40PlayersDataBase
             IsBanned=true;
         }
 
-        public void Unbun()
+        public void Unban()
         {
             IsBanned=false;
         }
 
         internal Player Clone()
         {
-            Player clonedPlayer = new(Id, Name);
-            clonedPlayer.IsBanned = IsBanned;
-            return clonedPlayer;
+            return new Player(_id, Name, IsBanned); 
         }
     }
 
     public class Database
     {
         private int _lastId = 0;
-        private readonly List<Player> Players = new ();
+        private List<Player> _players = new List<Player>();
 
         public void Add(string name)
         {
             _lastId++;
-            Player player = new(_lastId, name);
-            Players.Add(player);
+            Player player = new Player(_lastId, name);
+            _players.Add(player);
         }
 
         public void Remove(Player player)
         {
-            Players.Remove(player);
+            _players.Remove(player);
         }
 
         public List<Player> GetAll()
         {
-            List<Player> players = new();
+            List<Player> players = new List<Player>();
 
-            foreach (Player player in Players)
+            foreach (Player player in _players)
             {
                 Player clonedPlayer = (Player) player.Clone();
                 players.Add(clonedPlayer);
@@ -76,13 +82,69 @@ namespace Task40PlayersDataBase
 
         public Player Find(int id)
         {
-            return Players.FirstOrDefault(player => player.Id == id);
+            return _players.FirstOrDefault(player => player.Id == id);
         }
     }
 
 class ConsoleControl
     {
-        private Player GetPlayer(Database players)
+        public void Run()
+        {
+            const char CommandAddPlayer = '1';
+            const char CommandBanPlayer = '2';
+            const char CommandUnbanPlayer = '3';
+            const char CommandDeletePlayer = '4';
+            const char CommandShowPlayer = '5';
+            const char CommandExit = '6';
+
+            Database players = new Database();
+            bool isRunning = true;
+
+            while (isRunning)
+            {
+                Console.WriteLine($"выберите действие:\n{CommandAddPlayer}-добавить игрока\n{CommandBanPlayer}-забанить игрока\n" +
+                    $"{CommandUnbanPlayer}-разбанить игрока\n{CommandDeletePlayer}-удалить игрока\n{CommandShowPlayer}-вывести всех игроков\n{CommandExit}-выход");
+                char key = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+
+                switch (key)
+                {
+                    case CommandAddPlayer:
+                        AddPlayer(players);
+                        break;
+
+                    case CommandBanPlayer:
+                        TryGetPlayer(players)?.Ban();
+                        break;
+
+                    case CommandUnbanPlayer:
+                        TryGetPlayer(players)?.Unban();
+                        break;
+
+                    case CommandDeletePlayer:
+                        RemovePlayer(players);
+                        break;
+
+                    case CommandShowPlayer:
+                        ShowPlayersInfo(players);
+                        break;
+
+                    case CommandExit:
+                        isRunning = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Неверно выбрано действие");
+                        break;
+                }
+
+                Console.WriteLine("для продолжения нажмите любую кнопку");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        private Player TryGetPlayer(Database players)
         {
             Console.WriteLine("Введите id: ");
             Int32.TryParse(Console.ReadLine(), out int id);
@@ -115,66 +177,11 @@ class ConsoleControl
 
         private void RemovePlayer(Database players)
         {
-            Player player = GetPlayer(players);
+            Player player = TryGetPlayer(players);
 
             if (player != null)
             {
                 players.Remove(player);
-            }
-        }
-
-        public void Run()
-        {
-            const char CommandAddPlayer = '1';
-            const char CommandBanPlayer = '2';
-            const char CommandUnbanPlayer = '3';
-            const char CommandDeletePlayer = '4';
-            const char CommandShowPlayer = '5';
-            const char CommandExit = '6';
-            Database players = new();
-            bool isRunning = true;
-
-            while (isRunning)
-            {
-                Console.WriteLine($"выберите действие:\n{CommandAddPlayer}-добавить игрока\n{CommandBanPlayer}-забанить игрока\n" +
-                    $"{CommandUnbanPlayer}-разбанить игрока\n{CommandDeletePlayer}-удалить игрока\n{CommandShowPlayer}-вывести всех игроков\n{CommandExit}-выход");
-                char key = Console.ReadKey().KeyChar;
-                Console.WriteLine();
-
-                switch (key)
-                {
-                    case CommandAddPlayer:
-                        AddPlayer(players);
-                        break;
-
-                    case CommandBanPlayer:
-                        GetPlayer(players)?.Ban();
-                        break;
-
-                    case CommandUnbanPlayer:
-                        GetPlayer(players)?.Unbun();
-                        break;
-
-                    case CommandDeletePlayer:
-                        RemovePlayer(players);
-                        break;
-
-                    case CommandShowPlayer:
-                        ShowPlayersInfo(players);
-                        break;
-
-                    case CommandExit:
-                        isRunning = false;
-                        break;
-
-                    default:
-                        Console.WriteLine("Неверно выбрано действие");
-                        break;
-                }
-
-                Console.WriteLine("для продолжения нажмите любую кнопку");
-                Console.ReadKey();
-                Console.Clear();
             }
         }
     }
